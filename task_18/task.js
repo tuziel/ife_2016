a898c8150d28c7d633dc3ea35e24d46f185a5237
tuziel <tuziel@outlook.com>
2017-08-07 23:27:05 +0800

    task_18: 添加注释

--- task_18/task.js
+++ task_18/task.js
@@ -1,8 +1,18 @@
+/**
+ * 生成队列
+ * @param {array} data
+ * @param {HTMLElement} elm
+ */
 var list = (function (data, elm) {
 	var data = data || [];
 	var elm = elm || null;
 
 	return {
+		/**
+		 * 从队列右边插入元素
+		 * 返回数组长度
+		 * @param {any} val
+		 */
 		push: function (val) {
 			var item = document.createElement("span");
 			item.innerHTML = val;
@@ -10,12 +20,23 @@ var list = (function (data, elm) {
 			console.log(data);
 			return data.push(val);
 		},
+
+		/**
+		 * 从队列右边删除元素
+		 * 返回删除的元素
+		 */
 		pop: function () {
 			var lastItem = elm.lastElementChild;
 			lastItem && elm.removeChild(lastItem);
 			console.log(data);
 			return data.pop();
 		},
+
+		/**
+		 * 从队列左边插入元素
+		 * 返回数组长度
+		 * @param {any} val
+		 */
 		unshift: function (val) {
 			var item = document.createElement("span"),
 				firstItem = elm.firstElementChild;
@@ -24,24 +45,42 @@ var list = (function (data, elm) {
 			console.log(data);
 			return data.unshift(val);
 		},
+
+		/**
+		 * 从队列左边删除元素
+		 * 返回删除的元素
+		 */
 		shift: function () {
 			var firstItem = elm.firstElementChild;
 			firstItem && elm.removeChild(firstItem);
 			console.log(data);
 			return data.shift();
 		},
-		remove(i) {
-			var item = elm.children[i];
-			elm.removeChild(item);
-			return data.splice(i, 1);
-		},
-		splice: function () {
 
+		/**
+		 * 删除索引位置的元素
+		 * 返回删除的元素
+		 * @param {number} index
+		 */
+		remove(index) {
+			var item = elm.children[index];
+			elm.removeChild(item);
+			return data.splice(index, 1);
 		},
+
+		splice: function () { },
+
+		/**
+		 * 渲染页面
+		 */
 		render: function () {
+			var data_ = data;
+
+			// 清空页面与数据
 			elm.innerHTML = "";
-			data_ = data;
 			data = [];
+
+			// 插入数据渲染页面
 			data_.forEach(function (i) {
 				this.push(i);
 			}, this);
@@ -59,15 +98,23 @@ var list = (function (data, elm) {
 		btnUnshift = document.getElementById("list-unshift"),
 		btnShift = document.getElementById("list-shift");
 
+	/**
+	 * 获取输入的数据
+	 */
 	function getInputVal() {
 		var val = document.getElementById("list-input").value;
+
 		if (!val) {
 			alert("请输入数值");
 			return false;
 		}
+
 		return val;
 	}
 
+	/**
+	 * 列表元素的点击事件
+	 */
 	dom.onclick = function (ev) {
 		var item = ev.target,
 			index;
@@ -78,6 +125,9 @@ var list = (function (data, elm) {
 		}
 	}
 
+	/**
+	 * 按钮元素的点击事件
+	 */
 	btnPush.onclick = function () {
 		var val = getInputVal();
 		val && list.push(val);
@@ -95,6 +145,7 @@ var list = (function (data, elm) {
 		alert(val ? val : "队列为空");
 	}
 
+	// 渲染页面
 	list.render();
 
 })();
